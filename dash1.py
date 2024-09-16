5511b2aa8c59d2072d89fbf2467b8eb797150e50
anushreehmm <136401184+anushreehmm@users.noreply.github.com>
2024-09-16 15:25:26 +0530

    Update dash1.py

--- dash1.py
+++ dash1.py
@@ -161,4 +161,5 @@ def update_graphs(selected_hostnames):
     return packet_loss_fig, latency_fig, availability_fig
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=int(os.environ.get("PORT", 8080)))
+    app.run_server(host='0.0.0.0', port=8080)
+
